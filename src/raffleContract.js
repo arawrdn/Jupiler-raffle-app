6163d744cd8f546e1b7c4bec2667c667f218b143
0xward <aradeawardana97@gmail.com>
2025-09-29 01:40:02 +0700

    Update raffleContract.js

--- src/raffleContract.js
+++ src/raffleContract.js
@@ -1,1 +1,11 @@
+import { ethers } from "ethers";
+
+export const JupilerRaffleABI = [
+  "function enterRaffle()",
+  "function pickWinner()",
+  "function fundRaffle() payable",
+  "function getPlayers() view returns (address[])"
+];
+
+export const raffleAddress = "0x2A6b5204B83C7619c90c4EB6b5365AA0b7d912F7"; // Ganti dengan alamat kontrak
 
